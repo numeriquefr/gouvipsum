1c7a8bc9e08a04533826433ba4364f0d82d33817
numeriquefr <nope@service-public.fr>
2020-05-02 12:41:21 -0400

    Tweet random content

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 import random
+import os
+
+import tweepy
+
+
+class Twitter(object):
+    def __init__(self):
+        super(Twitter, self).__init__()
+        auth = tweepy.OAuthHandler(
+            os.environ["TWITTER_API_KEY"], os.environ["TWITTER_API_SECRET"]
+        )
+        auth.set_access_token(
+            os.environ["TWITTER_ACCESS_TOKEN"],
+            os.environ["TWITTER_ACCESS_TOKEN_SECRET"],
+        )
+
+        self.api = tweepy.API(auth)
+        self.api.verify_credentials()
+
+    def tweet(self, text):
+        return self.api.update_status(text)
 
 
 with open("words.txt") as f:
     words = set([l.strip() for l in f.readlines()])
 
-for _ in range(50):
-    nb = random.randint(3, 5)
-    print(" ".join(random.sample(words, nb)))
+nb_words = random.randint(3, 5)
+sentence = " ".join(random.sample(words, nb_words))
+Twitter().tweet(sentence)
